2c4a132d6813ea50804c229048136a7c435ec6ac
Nick Kravcenko <nkr@even-on-sunday.com>
2018-03-29 15:16:15 +0200

    Add Bootstrap 4

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer>
+    footer 2
+</footer>
--- index.php
+++ index.php
@@ -5,14 +5,27 @@
     <title>Gulp, Sass, ES6</title>
     <link type="text/css" rel="stylesheet" href="./public/css/style.css" media="screen,projection" />
 </head>
-<body>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript" src="./public/js/main.js"></script>
+    <body>
+        <header>
+            <div class="row">
+                <div class="col-3">
+                    <div class="card">
+                        <img class="card-img-top" src="http://via.placeholder.com/350x150" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
 
-<div style="background-color:lime;">
-    lorem ipsum dolor
-</div>
+        <?php include 'footer.php'; ?>
 
-</body>
-
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="./public/js/main.js"></script>
+    </body>
 </html>
